168c7f7e0c10129a12c80f3654075be80fa50077
Sukhmandeep-Maan <85410928+Sukhmandeep-Maan@users.noreply.github.com>
2021-11-29 22:11:30 +0530

    Update urls.py

--- QRCode/urls.py
+++ QRCode/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('send', views.home),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
