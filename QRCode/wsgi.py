dfc7abfd518182bb2da08ca9137e967e7e870402
Sukhmandeep-Maan <85410928+Sukhmandeep-Maan@users.noreply.github.com>
2021-11-29 21:51:34 +0530

    Update wsgi.py

--- QRCode/wsgi.py
+++ QRCode/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QRcode.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QRCode.settings')
 
 application = get_wsgi_application()
