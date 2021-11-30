06e1116e4796a2509cc1bb28ae5a0a39ec0f2377
Sukhmandeep-Maan <85410928+Sukhmandeep-Maan@users.noreply.github.com>
2021-11-30 21:33:46 +0530

    adding import settings

--- QRCode/urls.py
+++ QRCode/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from QRCode import views
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
